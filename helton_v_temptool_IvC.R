2b310e47d44f18de3e317c560df10327da06c466
skfogg <s.katie.fogg@gmail.com>
2021-11-29 16:59:01 -0700

    Finalize TempTool model validation against Helton's data. Pretty plots for publication.

--- helton_v_temptool_IvC.R
+++ helton_v_temptool_IvC.R
@@ -8,10 +8,13 @@
 
 
 
-load("temptool_output/tszHeatOut_high.RData")
-load("temptool_output/tszHeatOut_little.RData")
+# load("temptool_output/tszHeatOut_high.RData")
+# load("temptool_output/tszHeatOut_little.RData")
 load("temptool_output/highhypo870.RData")
 load("temptool_output/littlehypo870.RData")
+helton <- read.csv("Helton_2014_Figure_5.csv")
+highhypobins <- TSZStats(18, 60, 182*86400, 35.94, factor = 2, alpha = 1.39)
+
 
 timeindex <- index(highhypo$cTemp2)
 
@@ -20,18 +23,18 @@ zooit <- function(x,t){
 }
 
 #### HEAT SHIT #####
-heatout_high <- lapply(tszHeatOut_high, zooit, t = timeindex)
-heatout_little <- lapply(tszHeatOut_little, zooit, t = timeindex)
-
-plot.zoo(heatout_high[[1]]["2016"], ylim = c(0,50))
-mapply(function(x,c) lines(as.zoo(x["2016"]), col = c),
-       heatout_high,
-       hcl.colors(18))
-
-plot.zoo(heatout_little[[1]]["2016"], ylim = c(0,50))
-mapply(function(x,c) lines(as.zoo(x["2016"]), col = c),
-       heatout_little,
-       hcl.colors(18))
+# heatout_high <- lapply(tszHeatOut_high, zooit, t = timeindex)
+# heatout_little <- lapply(tszHeatOut_little, zooit, t = timeindex)
+#
+# plot.zoo(heatout_high[[1]]["2016"], ylim = c(0,50))
+# mapply(function(x,c) lines(as.zoo(x["2016"]), col = c),
+#        heatout_high,
+#        hcl.colors(18))
+#
+# plot.zoo(heatout_little[[1]]["2016"], ylim = c(0,50))
+# mapply(function(x,c) lines(as.zoo(x["2016"]), col = c),
+#        heatout_little,
+#        hcl.colors(18))
 
 #### TEMP SHIT ####
 plot.zoo(highHypo870[[2]]$svValue["2016"], ylim = c(0,25))
@@ -66,10 +69,10 @@ findphase <- function(x){
 }
 
 ## Associate phase with residence time/water age
-little_phases <- data.frame(WA = littlehypobins$meanWaterAge, RT = littlehypobins$meanResTime, phaseDay = numeric(18))
-for(i in 2:19){
-  little_phases$phaseDay[i-1] <- findphase(little_dailymeans[[i-1]]$svValue)
-}
+# little_phases <- data.frame(WA = littlehypobins$meanWaterAge, RT = littlehypobins$meanResTime, phaseDay = numeric(18))
+# for(i in 2:19){
+#   little_phases$phaseDay[i-1] <- findphase(little_dailymeans[[i-1]]$svValue)
+# }
 
 high_phases <- data.frame(WA = highhypobins$meanWaterAge, RT = highhypobins$meanResTime, phaseDay = numeric(18))
 for(i in 2:19){
@@ -77,21 +80,32 @@ for(i in 2:19){
 }
 
 ## Calc Ranges
-little_ranges <- data.frame(WA = littlehypobins$meanWaterAge, RT = littlehypobins$meanResTime, range = numeric(18))
-for(i in 2:19){
-  little_ranges$range[i-1] <- round(max(little_dailymeans[[i-1]]$svValue["2016"]) - min(little_dailymeans[[i-1]]$svValue["2016"]), 2)
-}
+# little_ranges <- data.frame(WA = littlehypobins$meanWaterAge, RT = littlehypobins$meanResTime, range = numeric(18))
+# for(i in 2:19){
+#   little_ranges$range[i-1] <- round(max(little_dailymeans[[i-1]]$svValue["2016"]) - min(little_dailymeans[[i-1]]$svValue["2016"]), 2)
+# }
 
 high_ranges <- data.frame(WA = highhypobins$meanWaterAge, RT = highhypobins$meanResTime, range = numeric(18))
 for(i in 2:19){
   high_ranges$range[i-1] <- round(max(high_dailymeans[[i-1]]$svValue["2016"]) - min(high_dailymeans[[i-1]]$svValue["2016"]), 2)
 }
 
-little_phases_lm <- lm(phaseDay ~ WA, little_phases)
-summary(little_phases_lm)
+# little_phases_lm <- lm(phaseDay ~ WA, little_phases)
+# summary(little_phases_lm)
 
-high_phases_lm <- lm(phaseDay ~ WA, high_phases)
+### Empirical models ####
+## LINEAR ###
+high_phases_lm <- lm(phaseDay ~ RT, high_phases)
 summary(high_phases_lm)
+
+high_ranges_lm <- lm(range ~ RT, high_ranges)
+summary(high_ranges_lm)
+
+high_phases_exp <- nls(phaseDay ~ y*exp(z*I(RT/86400)), data = high_phases, start = list(y = 210, z = 0.01) )
+summary(high_phases_exp)
+
+high_range_exp <- nls(range ~ y*exp(z*I(RT/86400)), data = high_ranges, start = list(y = 20, z = 0.01) )
+summary(high_range_exp)
 
 #######################################
 ## Re-Create Helton's Plot: Phase v. RT
@@ -117,67 +131,130 @@ dev.off()
 #######################################
 ## Re-Create Helton's Plot: Phase v. RT
 #######################################
-png("plots/phaselag_v_RT.png", width = 600*5, height = 600*5, res = 72*5)
-plot(I(phaseDay - little_phases_lm$coefficients[1]) ~ I(RT/86400), little_phases,
-     ylim = c(0.1, 100),
-     xlim = c(0, 182),
+png("plots/phaselag_v_RT_3.png", width = 580*5, height = 600*5, res = 72*5)
+plot(I(phaseDay - high_phases_lm$coefficients[1]) ~ I(RT/86400), high_phases,
+     ylim = c(0.1, 150),
+     xlim = c(0, 300),
      ylab = "Phase Lag (Days)",
      xlab = "Residence Time (Days)",
-     bg = "orange",
-     pch = 21,
-     main = "Phase Lag ~ Residence Time")
-points(I(phaseDay - high_phases_lm$coefficients[1]) ~ I(RT/86400), high_phases,
-       bg = "orangered3", pch = 21)
-# Helton's fit line
-lines((205*exp(1)^(0.0015*seq(0.01,182))-205), col = "dodgerblue")
-legend("topleft", c("little HE", "high HE", "Helton 2012 Best Fit Line"),
-       pch = c(21,21,NA), lty = c(NA, NA,1), col = c(1,1, "dodgerblue"),
-       pt.bg = c("orange", "orangered3", NA))
+     bg = "orangered3",
+     pch = 23,
+     cex = 1.3)
+points(I(Phase-205) ~ Avg_Res_Time_days, subset(helton, Model_Layer == "surface"), pch = 22, bg = "lightblue", col = "dodgerblue", cex = 1.3)
+points(I(Phase-205) ~ Avg_Res_Time_days, subset(helton, Model_Layer != "surface"), pch = 21, bg = "lightblue", col = "dodgerblue", cex = 1.3)
+
+lines((205*exp(1)^(0.0015*seq(0.01,300))-205), col = "dodgerblue", lwd = 2)
+legend("topleft", c("High HE Scenario", "Helton data & best fit line"),
+       pch = c(23,21), lty = c(NA,1), col = c(1, "dodgerblue"),
+       pt.bg = c("orangered3", "lightblue"), lwd = c(NA, 2))
 dev.off()
 
 
 #######################################
 ## Re-Create Helton's Plot: Phase v. RT
 #######################################
-png("plots/phaselag_v_logRT.png", width = 600*5, height = 600*5, res = 72*5)
-plot(I(phaseDay - little_phases_lm$coefficients[1]) ~ I(RT/86400), little_phases,
-     ylim = c(0.1, 80),
-     xlim = c(0.001, 182),
-     ylab = "Phase Lag (Days)",
-     xlab = "Residence Time (log(Days))",
-     bg = "orange",
-     pch = 21,
-     log = "x",
-     main = "Phase Lag ~ log(Residence Time)")
-points(I(phaseDay - high_phases_lm$coefficients[1]) ~ I(RT/86400), high_phases,
-       bg = "orangered3", pch = 21)
-# Helton's fit line
-lines((205*exp(1)^(0.0015*seq(0.01,182))-205), col = "dodgerblue")
-legend("topleft", c("little HE", "high HE", "Helton 2012 Best Fit Line"),
-       pch = c(21,21,NA), lty = c(NA, NA,1), col = c(1,1, "dodgerblue"),
-       pt.bg = c("orange", "orangered3", NA))
-dev.off()
+# png("plots/phaselag_v_logRT.png", width = 600*5, height = 600*5, res = 72*5)
+# plot(I(phaseDay - little_phases_lm$coefficients[1]) ~ I(RT/86400), little_phases,
+#      ylim = c(0.1, 80),
+#      xlim = c(0.001, 182),
+#      ylab = "Phase Lag (Days)",
+#      xlab = "Residence Time (log(Days))",
+#      bg = "orange",
+#      pch = 21,
+#      log = "x",
+#      main = "Phase Lag ~ log(Residence Time)")
+# points(Phase ~ Avg_Res_Time_days, helton, pch = 21, bg = "lightblue", col = "dodgerblue")
+# points(I(phaseDay - high_phases_lm$coefficients[1]) ~ I(RT/86400), high_phases,
+#        bg = "orangered3", pch = 21)
+# # Helton's fit line
+# lines((205*exp(1)^(0.0015*seq(0.01,182))-205), col = "dodgerblue")
+# legend("topleft", c("little HE", "high HE", "Helton 2012 Best Fit Line"),
+#        pch = c(21,21,NA), lty = c(NA, NA,1), col = c(1,1, "dodgerblue"),
+#        pt.bg = c("orange", "orangered3", NA))
+# dev.off()
 
 ########################################
 ## Re-Create Helton's Plot: Range v. RT
 ########################################
-png("plots/range_v_RT.png", width = 600*5, height = 600*5, res = 72*5)
-plot(range ~ I(WA/86400),
-     little_ranges,
-     ylim = c(0, 25),
-     xlim = c(0, 182),
-     ylab = "Temperature Range (C)",
+
+
+png("plots/range_v_RT_3.png", width = 600*5, height = 600*5, res = 72*5)
+plot(I(range - high_ranges_lm$coefficients[1]) ~ I(RT/86400),
+     high_ranges,
+     ylim = c(-14,0),
+     xlim = c(0, 300),
+     ylab = expression(paste(Delta, "Temperature Range ( ", degree,"C)")),
      xlab = "Residence Time (Days)",
-     bg = "orange",
-     pch = 21,
-     main = "Range ~ Residence Time")
-points(range~I(WA/86400), high_ranges, pch = 21, bg = "orangered3")
-lines(16.2*exp(1)^(-0.0065*seq(1,182)), col = "dodgerblue")
-legend("topright", c("little HE", "high HE", "Helton 2012 Best Fit Line"),
-       pch = c(21,21,NA), lty = c(NA, NA,1), col = c(1,1, "dodgerblue"),
-       pt.bg = c("orange", "orangered3", NA))
+     bg = "orangered3",
+     pch = 23,
+     cex = 1.3)
+points(I(FitRange-16.2) ~ Avg_Res_Time_days, subset(helton, Model_Layer == "surface"), pch = 22, col = "dodgerblue", bg = "lightblue", cex = 1.3)
+points(I(FitRange-16.2) ~ Avg_Res_Time_days, subset(helton, Model_Layer != "surface"), pch = 21, col = "dodgerblue", bg = "lightblue", cex = 1.3)
+lines((16.2*exp(1)^(-0.0065*seq(1,300)) - 16.2), col = "dodgerblue", lwd = 2)
+legend("topright", c("High HE Scenario", "Helton data & best fit line"),
+       pch = c(23,21), lty = c(NA,1), col = c(1, "dodgerblue"),
+       pt.bg = c("orangered3", "lightblue"), lwd = c(NA, 2))
+
+
 dev.off()
 
 
+### BOTH ####
+pt.cex <- 2.5
+png("plots/phaselag_range_v_RT_2.png", width = 580*5, height = 1200*5, res = 72*5)
+par(mfrow = c(2,1),
+    mar = c(1,5,1,1),
+    oma = c(4,0,0,0),
+    bty = "l",
+    cex.axis = 1.8,
+    cex.lab = 2)
+plot(I(phaseDay - high_phases_lm$coefficients[1]) ~ I(RT/86400), high_phases,
+     ylim = c(0.1, 160),
+     xlim = c(0, 305),
+     ylab = expression(paste(Delta, " Day of Annual Maximum Daily Mean Temperature")),
+     xlab = "Residence Time (Days)",
+     bg = "orangered3",
+     pch = 23,
+     cex = pt.cex,
+     xaxt = "n")
+points(I(Phase-205) ~ Avg_Res_Time_days, subset(helton, Model_Layer == "surface"), pch = 22, bg = "lightblue", col = "dodgerblue", cex = pt.cex)
+points(I(Phase-205) ~ Avg_Res_Time_days, subset(helton, Model_Layer != "surface"), pch = 21, bg = "lightblue", col = "dodgerblue", cex = pt.cex)
+lines((205*exp(1)^(0.0015*seq(0.01,305))-205), col = "dodgerblue", lwd = 3)
+points(I(phaseDay - high_phases_lm$coefficients[1]) ~ I(RT/86400), high_phases,
+       bg = "orangered3",
+       pch = 23,
+       cex = pt.cex)
+lines((209*exp(0.0031*seq(0.01,305))-209), col = "black", lwd = 2)
+legend("top", c("High HE Scenario", "Nyack Floodplain"),
+       pch = c(23,21), lty = c(NA,1), col = c(1, "dodgerblue"),
+       pt.bg = c("orangered3", "lightblue"), lwd = c(NA, 3),
+       bty = "n",
+       cex = 2.5)
+# legend("topleft", c("High HE Scenario", "Helton data & best fit line"),
+#        pch = c(23,21), lty = c(NA,1), col = c(1, "dodgerblue"),
+#        pt.bg = c("orangered3", "lightblue"), lwd = c(NA, 2))
+axis(1, at = c(0,50,100,150,200,250,300), labels = T)
 
+
+plot(I(range - high_ranges_lm$coefficients[1]) ~ I(RT/86400),
+     high_ranges,
+     ylim = c(-17,3),
+     xlim = c(0, 305),
+     ylab = expression(paste(Delta, "Temperature Range ( ", degree,"C)")),
+     xlab = "Residence Time (Days)",
+     bg = "orangered3",
+     pch = 23,
+     cex = pt.cex)
+points(I(FitRange-16.2) ~ Avg_Res_Time_days, subset(helton, Model_Layer == "surface"), pch = 22, col = "dodgerblue", bg = "lightblue", cex = pt.cex)
+points(I(FitRange-16.2) ~ Avg_Res_Time_days, subset(helton, Model_Layer != "surface"), pch = 21, col = "dodgerblue", bg = "lightblue", cex = pt.cex)
+lines((16.2*exp(1)^(-0.0065*seq(1,305)) - 16.2), col = "dodgerblue", lwd = 3)
+points(I(range - high_ranges_lm$coefficients[1]) ~ I(RT/86400),
+       high_ranges,
+       bg = "orangered3",
+       pch = 23,
+       cex = pt.cex)
+lines((19.3*exp(-0.0025*seq(0.01,305))-19.3), col = "black", lwd = 2)
+
+mtext("Hydrologic Residence Time (Days)", outer = T, side = 1, line = 2, cex = 2)
+ dev.off()
 
